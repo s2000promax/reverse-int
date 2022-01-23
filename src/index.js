931216f27479f9e579fe7634a576e66fb709fcc3
Stan <s2000ProMax@gmail.com>
2022-01-23 22:02:38 +0300

    update: update code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    
+      let string = Math.abs(number) +'';
+  number = '';
+
+  for (let index = 0; index < string.length; index += 1) {
+    number += string[string.length - 1 - index]
+  }
+
+  return number;
   
 }
